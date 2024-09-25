00f0e6991c72cd8748a996ef4d87b20e83f751dd
Landon Schropp <schroppl@gmail.com>
2024-09-24 22:52:25 -0700

    Add icons to commands

--- main.ts
+++ main.ts
@@ -12,31 +12,35 @@ export default class ListCyclerPlugin extends Plugin {
     this.addCommand({
       id: "cycle-list-item-forward",
       name: "Cycle List Item Forward",
+      icon: "forward",
       editorCallback: cycleListItemForward,
     });
 
     this.addCommand({
       id: "cycle-list-item-backward",
       name: "Cycle List Item Backward",
+      icon: "backward",
       editorCallback: cycleListItemBackward,
     });
 
     this.addCommand({
       id: "cycle-checkbox-forward",
       name: "Cycle Checkbox Forward",
+      icon: "square-arrow-right",
       editorCallback: (editor) => cycleCheckboxForward(editor, this.settings.checkboxes),
     });
 
     this.addCommand({
       id: "cycle-checkbox-backward",
       name: "Cycle Checkbox Backward",
+      icon: "square-arrow-left",
       editorCallback: (editor) => cycleCheckboxBackward(editor, this.settings.checkboxes),
     });
 
     this.addSettingTab(new ListCyclerSettingTab(this.app, this));
   }
 
-  onunload() {}
+  onunload() { }
 
   async loadSettings() {
     this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
