217bbb874804a80535f9c403139cfd5310b251cd
Landon Schropp <schroppl@gmail.com>
2024-10-06 23:11:40 -0700

    Add scaffold of list item settings

--- group-settings.ts
+++ group-settings.ts
@@ -1,6 +1,11 @@
+import { ListItemSettings } from "list-item-settings";
 import { Setting } from "obsidian";
 import { Settings } from "settings";
 import { GroupSettings as GroupSettingsType, Settings as SettingsType } from "types";
+
+const EMPTY_LIST_ITEM = {
+  text: "",
+};
 
 /** The settings for a single group. */
 export class GroupSettings {
@@ -51,12 +56,20 @@ export class GroupSettings {
     await this.settingsTab.setSettingsAndRerender(this.mergeGroupSettings(undefined));
   }
 
+  async addListItem() {
+    await this.setSettingsAndRerender({
+      ...this.settings,
+      listItems: [...this.settings.listItems, structuredClone(EMPTY_LIST_ITEM)],
+    });
+  }
+
   display(): void {
     // Temporary Separator between groups
     this.containerEl.createEl("br");
     this.containerEl.createEl("br");
     this.containerEl.createEl("br");
 
+    // Name Input
     new Setting(this.containerEl)
       .setName("Name")
       .setDesc("The name of the group")
@@ -79,5 +92,18 @@ export class GroupSettings {
           .setWarning()
           .onClick(() => this.deleteAndRerender());
       });
+
+    // Add button
+    new Setting(this.containerEl)
+      .setName("Add List Item")
+      .setDesc("Add a new list item to cycle through")
+      .addButton((button) => {
+        button.setButtonText("Add List Item").onClick(() => this.addListItem());
+      });
+
+    // List items
+    for (let index = 0; index < this.settings.listItems.length; index++) {
+      new ListItemSettings(this, index).display();
+    }
   }
 }
---
+++ list-item-settings.ts
@@ -0,0 +1,104 @@
+import { ExtraButtonComponent, Setting } from "obsidian";
+import { GroupSettings } from "./group-settings";
+import {
+  ListItemSettings as ListItemSettingsType,
+  GroupSettings as GroupSettingsType,
+} from "./types";
+
+/** The settings for a single group. */
+export class ListItemSettings {
+  groupSettings: GroupSettings;
+  index: number;
+
+  constructor(groupSettings: GroupSettings, index: number) {
+    this.groupSettings = groupSettings;
+    this.index = index;
+  }
+
+  get app() {
+    return this.groupSettings.app;
+  }
+
+  get plugin() {
+    return this.groupSettings.plugin;
+  }
+
+  get containerEl() {
+    return this.groupSettings.containerEl;
+  }
+
+  get settings() {
+    return this.groupSettings.settings.listItems[this.index];
+  }
+
+  get isFirstListItem() {
+    return this.index === 0;
+  }
+
+  get isLastListItem() {
+    return this.index === this.groupSettings.settings.listItems.length - 1;
+  }
+
+  get isOnlyListItem() {
+    return this.groupSettings.settings.listItems.length <= 1;
+  }
+
+  mergeListItemSettings(settings: ListItemSettingsType | undefined): GroupSettingsType {
+    return {
+      ...this.groupSettings.settings,
+      listItems: [
+        ...this.groupSettings.settings.listItems.slice(0, this.index),
+        settings,
+        ...this.groupSettings.settings.listItems.slice(this.index + 1),
+      ].filter((group) => group !== undefined),
+    };
+  }
+
+  async setSettings(settings: ListItemSettingsType): Promise<void> {
+    await this.groupSettings.setSettings(this.mergeListItemSettings(settings));
+  }
+
+  async setSettingsAndRerender(settings: ListItemSettingsType): Promise<void> {
+    await this.groupSettings.setSettingsAndRerender(this.mergeListItemSettings(settings));
+  }
+
+  async deleteAndRerender() {
+    await this.groupSettings.setSettingsAndRerender(this.mergeListItemSettings(undefined));
+  }
+
+  private styleExtraButton(
+    button: ExtraButtonComponent,
+    icon: string,
+    disabled: boolean,
+    callback: () => void,
+  ): void {
+    button.setIcon(icon).setDisabled(disabled).onClick(callback);
+    button.extraSettingsEl.style.cursor = disabled ? "not-allowed" : "pointer";
+    button.extraSettingsEl.style.opacity = disabled ? "50%" : "100%";
+    button.extraSettingsEl.style.pointerEvents = disabled ? "none" : "auto";
+  }
+
+  display(): void {
+    new Setting(this.containerEl)
+      .addText((text) => {
+        text
+          .setPlaceholder("List Item")
+          .setValue(this.settings.text)
+          .onChange((value) => this.setSettings({ ...this.settings, text: value }));
+
+        text.inputEl.style.marginRight = "auto";
+      })
+      .addExtraButton((button) => {
+        this.styleExtraButton(button, "arrow-up", this.isFirstListItem, () => {});
+      })
+      .addExtraButton((button) => {
+        this.styleExtraButton(button, "arrow-down", this.isLastListItem, () => {});
+      })
+      .addExtraButton((button) => {
+        button
+          .setIcon("trash")
+          .setDisabled(this.isOnlyListItem)
+          .onClick(() => this.deleteAndRerender());
+      });
+  }
+}
