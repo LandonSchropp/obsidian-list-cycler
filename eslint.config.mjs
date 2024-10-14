a782e98d011d655b0803518d28f5c8d06c6e7480
Landon Schropp <schroppl@gmail.com>
2024-10-13 23:39:10 -0700

    Add ESLint with an up-to-date configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import javascript from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
+import typescript from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["main.js"],
+  },
+  javascript.configs.recommended,
+  ...typescript.configs.recommended,
+  prettier,
+  {
+    files: ["**/*.{js,ts,mjs}"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+];
